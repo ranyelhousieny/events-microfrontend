b71104dea78180a155fc4fbc7cced20d0f6c2bfa
Rany ElHousieny <elhousie@gmail.com>
2021-09-07 20:59:12 -0700

    start

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+const template = (
+  <div>
+    <h1>===========</h1>
+    <h3>
+      Events Microfrontend
+    </h3>
+
+    <h1>===========</h1>
+  </div>
+);
+
+ReactDOM.render(
+  template,
+  document.querySelector(
+    '#fromjs'
+  )
+);
